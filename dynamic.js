2c6327965afea8d68e9524e74cfff37d42ecc729
Jaytex <jaybamimore@hotmail.com>
2014-10-09 20:55:33 -0400

    Documented dynamic.js

--- dynamic.js
+++ dynamic.js
@@ -1,4 +1,11 @@
 /*
+	JS file that deals with the dynamism
+	Author	: Jay Bamimore
+	Date	: 09 - 10 - 2014
+*/
+
+
+/*
 	getWidth() 	: Helper function for obtaining browser's width 
 
 	parameters	: None
@@ -8,13 +15,13 @@
 
 function getWidth(){
 
-		/* Checking what to return */
+		/* Checking what to return (various sanity checks) */
 
 		if (window.innerWidth){
 			return window.innerWidth; // This will return the width of the viewport in pixels
 		}	
 		else if (document.documentElement && document.documentElement.clientWidth != 0){
-			return document.documentElement.clientWidth; // 
+			return document.documentElement.clientWidth; 
 		}
 		else if (document.body){
 			return document.body.clientWidth;
@@ -27,23 +34,27 @@ function getWidth(){
 
 	parameters		: The name of the layout
 
-	return			: void
+	return			: None
 */
 
 function changeLayout(layoutname){
 	document.body.className = layoutname; 
-
 }
 
 
 /*
-	Main Function
+	dynamic() 	: Changes the layout of the page based on the width
+
+	parameters	: None
+
+	return		: None
+
+
 */
 
 function dynamic(){
 
 	var browserWidth = getWidth(); // get's the browser's width
-	var page = document.URL;
 
 	if (browserWidth < 680){
 		changeLayout("thin");
@@ -53,32 +64,30 @@ function dynamic(){
 		changeLayout("wide");
 	}
 
-	if (browserWidth > 950){
+	if (browserWidth > 950){ 
 		changeLayout("wider");
 	}
 }
 
 /*
-	
+	addEvent 	: attach an event handler  to the window
 
+	parameters	: object, type , function
+
+	return		: none
 */
 
 function addEvent( obj, type, fn ){ 
-
 	   if (obj.addEventListener){ 
-	      obj.addEventListener( type, fn, false );
+	      obj.addEventListener( type, fn, false ); // event to attach
 	   }
 	   else if (obj.attachEvent){ 
 	      obj["e"+type+fn] = fn; 
-
 	      obj[type+fn] = function(){ obj["e"+type+fn]( window.event ); } 
-
-	      obj.attachEvent( "on"+type, obj[type+fn] ); 
+	      obj.attachEvent( "on"+type, obj[type+fn] ); //bind function to an event 
 	   } 
-} 
-
-//load based on the effects of dynamic function
+	} 
+	
+// Handle window load and resize with dynamic function 
 addEvent(window, 'load', dynamic);
-
-//resize based the effects of dynamic function
 addEvent(window, 'resize', dynamic);
